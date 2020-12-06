f3df46370e56f7330be07d98abe42608518b869c
Nataliia S. Novikova <nanov94@yandex-team.ru>
2020-12-06 22:42:48 +0300

    Lazy loading was implemented

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { UserListDTO } from '../../client/DTOs/UserListDTO';
 import { UserDTO } from '../../client/DTOs/UserDTO';
 import { getUserList } from '../../client/ReqresClient';
 import { connect } from 'react-redux';
-import ListItem from '../ListItem/ListItem';
 import { addUserList } from '../../actions/userAction';
+import Loading from '../Loading/Loading';
+
+const ListItem = React.lazy(() => import('../ListItem/ListItem'));
 
 interface UserListStateToProps {
-    users: UserDTO[]
+    users: UserDTO[];
+}
+
+interface UserListState {
+    isLoading: boolean;
+    isError: boolean;
+    isFetching: boolean;
+    isNoUsers: boolean;
+    pageNumber: number;
 }
 
 interface UserListDispatchToProps {
@@ -16,45 +26,81 @@ interface UserListDispatchToProps {
 
 type UserListProps = UserListStateToProps & UserListDispatchToProps;
 
-class UserList extends Component<UserListProps> {
+class UserList extends Component<UserListProps, UserListState> {
     constructor(props: any) {
         super(props);
 
         this.state = {
             isLoading: true,
             isError: false,
+            isFetching: false,
+            isNoUsers: false,
+            pageNumber: 1,
         }
     }
 
     componentDidMount() {
-        getUserList()
-            .then((userList: UserListDTO) => {
-                this.props.addUserList(userList.data);
-            })
-            .catch((error: any) => {
-                console.log(`Server is not available! Error: ${error}.`);
-                this.setState((state) => ({
-                    ...state,
-                    isError: true,
-                }));
-            })
-            .finally(() => {
-                this.setState((state) => ({
-                    ...state,
-                    isLoading: false,
-                }));
-            });
+        this.getUsers();
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentDidUpdate() {
+        if (!this.state.isFetching) {
+            return;
+        }
+
+        this.getUsers();
+        this.setState((state) => ({ ...state, isFetching: false }));
+    }
+
+    getUsers = () => {
+        setTimeout(() => {
+            getUserList(this.state.pageNumber)
+                .then((userList: UserListDTO) => {
+                    if (userList.total_pages < userList.page && userList.data.length === 0) {
+                        this.setState((state) => ({ ...state, isNoUsers: true }));
+
+                        return;
+                    }
+
+                    this.props.addUserList(userList.data);
+                    this.setState((state) => ({ ...state, pageNumber: state.pageNumber + 1 }));
+                })
+                .catch((error: any) => {
+                    console.log(`Server is not available! Error: ${error}.`);
+                    this.setState((state) => ({
+                        ...state,
+                        isError: true,
+                    }));
+                })
+                .finally(() => {
+                    this.setState((state) => ({
+                        ...state,
+                        isLoading: false,
+                    }));
+                });
+        }, 1000);
+    };
+
+    handleScroll = () => {
+        if (Math.ceil(window.innerHeight + document.documentElement.scrollTop) < document.documentElement.offsetHeight
+            || this.state.isFetching) {
+            return;
+        }
+
+        this.setState((state) => ({ ...state, isFetching: true }));
     }
 
     render() {
         const { users } = this.props;
+        const { isLoading, isError, isNoUsers } = this.state;
 
-        if (!users.length) {
-            return <div> Loading ...</div>;
+        if (isLoading) {
+            return <Loading />;
         }
 
-        if (!users.length) {
-            return <div> Loading ...</div>;
+        if (isError) {
+            return <div> Error </div>;
         }
 
         return (
@@ -62,16 +108,19 @@ class UserList extends Component<UserListProps> {
                 {
                     users.map((user: UserDTO) => {
                         return (
-                            <ListItem
-                                key={user.id}
-                                img={user.avatar}
-                                title={`${user.first_name} ${user.last_name}`}
-                                additionalInfo={user.email}
-                            />);
+                            <div key={user.id}>
+                                <Suspense fallback={<div>Get more users</div>}>
+                                    <ListItem
+                                        img={user.avatar}
+                                        title={`${user.first_name} ${user.last_name}`}
+                                        additionalInfo={user.email}
+                                    />
+                                </Suspense>
+                            </div>
+                        )
                     })
                 }
-
-                <div>No more users</div>
+                {isNoUsers && <div>No more users</div>}
             </div>
         );
     }
