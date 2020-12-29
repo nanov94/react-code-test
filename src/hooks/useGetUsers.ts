7a22cc02d932a28535815059680dc679435425ac
Natalia Novikova <novikovans94@gmail.com>
2020-12-29 08:54:58 +0300

    Promise -> async await

--- src/hooks/useGetUsers.ts
+++ src/hooks/useGetUsers.ts
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { getUserList } from '../client/ReqresClient';
-import { UserListDTO } from '../client/DTOs/UserListDTO';
 
 const useGetUsers = (dispatchCallback: (data: any) => void, isFetching: boolean) => {
     const [pageNumber, setPageNumber] = useState(1);
@@ -10,26 +9,25 @@ const useGetUsers = (dispatchCallback: (data: any) => void, isFetching: boolean)
 
     useEffect(() => {
         if (isFetching) {
-            setTimeout(() => {
-                getUserList(pageNumber)
-                    .then((userList: UserListDTO) => {
-                        if (userList.total_pages < userList.page && userList.data.length === 0) {
-                            setIsNoUsers(true);
+            setTimeout(async () => {
+                try {
+                    const userList = await getUserList(pageNumber);
+                    if (userList.total_pages < userList.page && userList.data.length === 0) {
+                        setIsNoUsers(true);
 
-                            return;
-                        }
+                        return;
+                    }
 
-                        dispatchCallback(userList.data);
-                        setPageNumber(pageNumber + 1);
-                    })
-                    .catch((error: any) => {
-                        // eslint-disable-next-line no-console
-                        console.log(`Server is not available! Error: ${error}.`);
-                        setIsError(true);
-                    })
-                    .finally(() => {
-                        setIsLoading(false);
-                    });
+                    dispatchCallback(userList.data);
+                    setPageNumber(pageNumber + 1);
+
+                } catch (error) {
+                    // eslint-disable-next-line no-console
+                    console.log(`Server is not available! Error: ${error}.`);
+                    setIsError(true);
+                } finally {
+                    setIsLoading(false);
+                }
             }, 1000);
         }
     });
